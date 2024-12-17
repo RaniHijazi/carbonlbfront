78e972390b9491ef4a975f93b5c0bcfcb5252ceb
wadihissa1 <202111259@ua.edu.lb>
2024-12-17 21:21:09 +0200

    Update Contact.js

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,146 +1,119 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
+import { Link } from "react-router-dom";
+import { logoLight } from "../../assets/images";
+import { BsCheckCircleFill } from "react-icons/bs";
+import { FaWhatsapp, FaInstagram } from "react-icons/fa";
 
 const Contact = () => {
   const location = useLocation();
   const [prevLocation, setPrevLocation] = useState("");
+
   useEffect(() => {
-    setPrevLocation(location.state.data);
+    setPrevLocation(location.state?.data || "Home");
   }, [location]);
 
-  const [clientName, setclientName] = useState("");
-  const [email, setEmail] = useState("");
-  const [messages, setMessages] = useState("");
+  const phoneNumber = "+961 71 015 151"; // Replace with your company's phone number
+  const whatsappNumber = "71015151"; // Replace with your company's WhatsApp number
+  const instagramUsername = "carbon_lb"; // Replace with your company's Instagram username
 
-  // ========== Error Messages Start here ============
-  const [errClientName, setErrClientName] = useState("");
-  const [errEmail, setErrEmail] = useState("");
-  const [errMessages, setErrMessages] = useState("");
-  // ========== Error Messages End here ==============
-  const [successMsg, setSuccessMsg] = useState("");
-
-  const handleName = (e) => {
-    setclientName(e.target.value);
-    setErrClientName("");
-  };
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-    setErrEmail("");
-  };
-  const handleMessages = (e) => {
-    setMessages(e.target.value);
-    setErrMessages("");
+  const handleWhatsAppRedirect = () => {
+    window.open(`https://wa.me/${whatsappNumber}`, "_blank");
   };
 
-  // ================= Email Validation start here =============
-  const EmailValidation = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
-  };
-  // ================= Email Validation End here ===============
-
-  const handlePost = (e) => {
-    e.preventDefault();
-    if (!clientName) {
-      setErrClientName("Enter your Name");
-    }
-    if (!email) {
-      setErrEmail("Enter your Email");
-    } else {
-      if (!EmailValidation(email)) {
-        setErrEmail("Enter a Valid Email");
-      }
-    }
-    if (!messages) {
-      setErrMessages("Enter your Messages");
-    }
-    if (clientName && email && EmailValidation(email) && messages) {
-      setSuccessMsg(
-        `Thank you dear ${clientName}, Your messages has been received successfully. Futher details will sent to you by your email at ${email}.`
-      );
-    }
+  const handleInstagramRedirect = () => {
+    window.open(`https://www.instagram.com/${instagramUsername}/`, "_blank");
   };
 
   return (
-    <div className="max-w-container mx-auto px-4">
-      <Breadcrumbs title="Contact" prevLocation={prevLocation} />
-      {successMsg ? (
-        <p className="pb-20 w-96 font-medium text-green-500">{successMsg}</p>
-      ) : (
-        <form className="pb-20">
-          <h1 className="font-titleFont font-semibold text-3xl">
-            Fill up a Form
-          </h1>
-          <div className="w-[500px] h-auto py-6 flex flex-col gap-6">
+    <div className="w-full h-screen flex">
+      <div className="w-1/2 h-full flex items-start pt-10">
+        <div className="max-w-container px-4 ml-10">
+          <Breadcrumbs title="Contact" prevLocation={prevLocation} />
+          <div className="w-full py-6 flex flex-col gap-6">
             <div>
-              <p className="text-base font-titleFont font-semibold px-2">
-                Name
+              <p className="text-base font-titleFont font-semibold px-2">Phone</p>
+              <p className="w-full py-1 px-2 text-base font-medium">
+                {phoneNumber}
               </p>
-              <input
-                onChange={handleName}
-                value={clientName}
-                className="w-full py-1 border-b-2 px-2 text-base font-medium placeholder:font-normal placeholder:text-sm outline-none focus-within:border-primeColor"
-                type="text"
-                placeholder="Enter your name here"
-              />
-              {errClientName && (
-                <p className="text-red-500 text-sm font-titleFont font-semibold mt-1 px-2 flex items-center gap-1">
-                  <span className="text-sm italic font-bold">!</span>
-                  {errClientName}
-                </p>
-              )}
             </div>
             <div>
-              <p className="text-base font-titleFont font-semibold px-2">
-                Email
-              </p>
-              <input
-                onChange={handleEmail}
-                value={email}
-                className="w-full py-1 border-b-2 px-2 text-base font-medium placeholder:font-normal placeholder:text-sm outline-none focus-within:border-primeColor"
-                type="email"
-                placeholder="Enter your name here"
-              />
-              {errEmail && (
-                <p className="text-red-500 text-sm font-titleFont font-semibold mt-1 px-2 flex items-center gap-1">
-                  <span className="text-sm italic font-bold">!</span>
-                  {errEmail}
-                </p>
-              )}
+              <button
+                onClick={handleWhatsAppRedirect}
+                className="w-56 bg-green-500 text-white h-12 font-titleFont text-base tracking-wide font-semibold hover:bg-green-700 duration-200 rounded-3xl flex items-center justify-center gap-2 whitespace-nowrap"
+              >
+                <FaWhatsapp className="text-3xl" />
+                Chat on WhatsApp
+              </button>
             </div>
             <div>
-              <p className="text-base font-titleFont font-semibold px-2">
-                Messages
-              </p>
-              <textarea
-                onChange={handleMessages}
-                value={messages}
-                cols="30"
-                rows="3"
-                className="w-full py-1 border-b-2 px-2 text-base font-medium placeholder:font-normal placeholder:text-sm outline-none focus-within:border-primeColor resize-none"
-                type="text"
-                placeholder="Enter your name here"
-              ></textarea>
-              {errMessages && (
-                <p className="text-red-500 text-sm font-titleFont font-semibold mt-1 px-2 flex items-center gap-1">
-                  <span className="text-sm italic font-bold">!</span>
-                  {errMessages}
-                </p>
-              )}
+              <button
+                onClick={handleInstagramRedirect}
+                className="w-56 bg-pink-600 text-white h-12 font-titleFont text-base tracking-wide font-semibold hover:bg-pink-700 duration-200 rounded-3xl flex items-center justify-center gap-2 whitespace-nowrap"
+              >
+                <FaInstagram className="text-3xl" />
+                DM on Instagram
+              </button>
             </div>
-            <button
-              onClick={handlePost}
-              className="w-44 bg-primeColor text-gray-200 h-10 font-titleFont text-base tracking-wide font-semibold hover:bg-black hover:text-white duration-200"
-            >
-              Post
-            </button>
           </div>
-        </form>
-      )}
+        </div>
+      </div>
+      <div className="w-1/2 hidden lgl:flex h-full text-white">
+        <div className="w-full h-full bg-primeColor px-10 flex flex-col gap-6 justify-center">
+          <Link to="/">
+            <p className="text-2xl font-bold text-white">Carbon Lb</p>
+          </Link>
+          <div className="flex flex-col gap-1 -mt-1">
+            <h1 className="font-titleFont text-xl font-medium">
+              Stay connected with us
+            </h1>
+            <p className="text-base">We are here to help you!</p>
+          </div>
+          <div className="w-full flex items-start gap-3">
+            <span className="text-green-500 mt-1">
+              <BsCheckCircleFill />
+            </span>
+            <p className="text-base text-gray-300">
+              <span className="text-white font-semibold font-titleFont">
+                Quick support
+              </span>
+              <br />
+              Contact us anytime for your inquiries and support needs.
+            </p>
+          </div>
+          <div className="w-full flex items-start gap-3">
+            <span className="text-green-500 mt-1">
+              <BsCheckCircleFill />
+            </span>
+            <p className="text-base text-gray-300">
+              <span className="text-white font-semibold font-titleFont">
+                Accessible everywhere
+              </span>
+              <br />
+              Reach us via phone or WhatsApp for convenience.
+            </p>
+          </div>
+          <div className="flex items-center justify-between mt-10">
+            <Link to="/">
+              <p className="text-sm font-titleFont font-semibold text-gray-300 hover:text-white cursor-pointer duration-300">
+                © CARBON_LB
+              </p>
+            </Link>
+            <p className="text-sm font-titleFont font-semibold text-gray-300 hover:text-white cursor-pointer duration-300">
+              Terms
+            </p>
+            <p className="text-sm font-titleFont font-semibold text-gray-300 hover:text-white cursor-pointer duration-300">
+              Privacy
+            </p>
+            <p className="text-sm font-titleFont font-semibold text-gray-300 hover:text-white cursor-pointer duration-300">
+              Security
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  );  
 };
 
 export default Contact;
